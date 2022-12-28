68c2a03085a5c3688d34690219377783ff91e1a2
twagner <twagner@student.42.fr>
2022-12-28 16:44:15 +0100

    progress: ex00 almost ok

--- module00/ex00/matrix.py
+++ module00/ex00/matrix.py
@@ -8,94 +8,201 @@ class Matrix():
         """Constructor"""
         # list constructor
         if isinstance(data, list):
+            m = len(data) if data is not None else 0
+            n = len(data[0]) if data is not None else 0
+            # check if all rows have the same length
+            if not all(len(row) == n for row in data):
+                raise ValueError(f'Wrong shape on value : {data}')
             self.data = data
-            if len(data) == 1:
-                self.shape = (1, len(data[0]))
-            else:
-                self.shape = (len(data), 1)
+            self.shape = (m, n)
         # shape constructor
         elif isinstance(data, tuple):
-            self.values = [[float(i)] for i in range(data[0], data[1])]
-            self.shape = (data[1] - data[0], 1)
+            # check if tuple is ok
+            if len(data) != 2 or data is None:
+                raise ValueError(f'Wrong shape: {data}')
+            if not isinstance(data[0], int) or not isinstance(data[1], int):
+                raise TypeError(f'Wrong type: {data}')
+            if data[0] < 0 or data[1] < 0:
+                raise ValueError(f'Dimensions of a shape must be positives')
+            # creation of the matrix
+            self.data = [[0 for n in range(data[1])] for m in range(data[0])]
+            self.shape = (data[0], data[1])
         else:
             raise TypeError(f'Wrong type : {type(data)}')
 
-    # def dot(self, v_2):
-    #     """Dot product"""
-    #     if v_2.shape[0] == self.shape[0] and v_2.shape[1] == self.shape[1]:
-    #         if self.shape[1] == 1:
-    #             return sum(self.values[i][0] * v_2.values[i][0]
-    #                        for i in range(len(self.values)))
-    #         return sum(self.values[0][i] * v_2.values[0][i]
-    #                    for i in range(len(self.values[0])))
-    #     raise AttributeError("Vectors don't have the same shape")
+    # Operators overload
+    def __add__(self, rhs):
+        """add: only matrices of same dimensions."""
+        if rhs.shape == self.shape:
+            try:
+                result = [[self.data[m][n] + rhs.data[m][n]
+                        for n in range(self.shape[1])]
+                        for m in range(self.shape[0])]
+            except TypeError:
+                result = [self.data[n] - rhs.data[n]
+                          for n in range(self.shape[1])]
+            return type(self)(result)
+        raise AttributeError("Shape error")
 
-    # def T(self):
-    #     """Transpose vector (row / column)"""
-    #     if self.shape[0] == 1:
-    #         return Vector([[value] for value in self.values[0]])
-    #     return Vector([[value[0] for value in self.values]])
+    def __radd__(self, rhs):
+        """radd: only matrices of same dimensions."""
+        return rhs + self
 
-    # # Operators overload
-    # def __add__(self, rhs):
-    #     if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
-    #         if self.shape[1] == 1:
-    #             return Vector([[self.values[i][0] + rhs.values[i][0]]
-    #                           for i in range(len(self.values))])
-    #         return Vector([[self.values[0][i] + rhs.values[0][i]
-    #                       for i in range(len(self.values[0]))]])
-    #     raise AttributeError("Vectors don't have the same shape")
+    def __sub__(self, rhs):
+        """sub: only matrices of same dimensions."""
+        if rhs.shape == self.shape:
+            try:
+                result = [[self.data[m][n] - rhs.data[m][n]
+                        for n in range(self.shape[1])]
+                        for m in range(self.shape[0])]
+            except TypeError:
+                result = [self.data[n] - rhs.data[n]
+                          for n in range(self.shape[1])]
+            return type(self)(result)
+        raise AttributeError("Shape error")
 
-    # def __radd__(self, rhs):
-    #     if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
-    #         if self.shape[1] == 1:
-    #             return Vector([[rhs.values[i][0] + self.values[i][0]]
-    #                           for i in range(len(self.values))])
-    #         return Vector([[rhs.values[0][i] + self.values[0][i]
-    #                         for i in range(len(self.values[0]))]])
-    #     raise AttributeError("Vectors don't have the same shape")
+    def __rsub__(self, rhs):
+        """rsub: only matrices of same dimensions."""
+        return rhs - self
 
-    # def __sub__(self, rhs):
-    #     if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
-    #         if self.shape[1] == 1:
-    #             return Vector([[self.values[i][0] - rhs.values[i][0]]
-    #                           for i in range(len(self.values))])
-    #         return Vector([[self.values[0][i] - rhs.values[0][i]
-    #                         for i in range(len(self.values[0]))]])
-    #     raise AttributeError("Vectors don't have the same shape")
+    def __truediv__(self, scalar):
+        """div: only scalars."""
+        if scalar == 0:
+            raise ArithmeticError("Division by 0.")
+        try:
+            result = [[self.data[m][n] / scalar for n in range(self.shape[1])]
+                    for m in range(self.shape[0])]
+        except TypeError:
+            result = [self.data[n] / scalar for n in range(self.shape[1])]
+        return type(self)(result)
 
-    # def __rsub__(self, rhs):
-    #     if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
-    #         if self.shape[1] == 1:
-    #             return Vector([[rhs.values[i][0] - self.values[i][0]]
-    #                           for i in range(len(self.values))])
-    #         return Vector([[rhs.values[0][i] - self.values[0][i]
-    #                         for i in range(len(self.values[0]))]])
-    #     raise AttributeError("Vectors don't have the same shape")
+    def __rtruediv__(self, scalar):
+        """rdiv: not implemented."""
+        raise ArithmeticError("Division of a scalar by a Matrix / Vector"
+                              " is not defined here.")
 
-    # def __truediv__(self, scalar):
-    #     if scalar == 0:
-    #         raise ArithmeticError("Division by 0.")
-    #     if self.shape[1] == 1:
-    #         return Vector([[value[0] / scalar] for value in self.values])
-    #     return Vector([[value / scalar for value in self.values[0]]])
+    def __mul__(self, rhs):
+        """mul: scalars, vectors and matrixes."""
+        try:
+            if isinstance(rhs, (int, float, complex)):
+                try:
+                    result = [[self.data[m][n] * rhs
+                               for n in range(self.shape[1])]
+                              for m in range(self.shape[0])]
+                except TypeError:
+                    result = [self.data[m] * rhs for m in range(self.shape[0])]
+                return type(self)(result)
 
-    # def __rtruediv__(self, scalar):
-    #     raise ArithmeticError("Division of a scalar by a Vector"
-    #                           " is not defined here.")
+            elif type(rhs) == Matrix and type(self) == Matrix:
+                # verify shape compatibility
+                if self.shape[1] != rhs.shape[0]:
+                    raise AttributeError("Shapes are not compatibles")
+                # compute result
+                result = [[sum(self.data[m][n] * rhs.data[n][p]
+                            for n in range(self.shape[1]))
+                           for p in range(rhs.shape[1])]
+                          for m in range(self.shape[0])]
+                return type(self)(result)
 
-    # def __mul__(self, scalar):
-    #     if self.shape[1] == 1:
-    #         return Vector([[value[0] * scalar] for value in self.values])
-    #     return Vector([[value * scalar for value in self.values[0]]])
+            elif type(rhs) == Vector and type(self) == Matrix:
+                # verify shape compatibility
+                if self.shape[1] != rhs.shape[0]:
+                    raise AttributeError("Shapes are not compatibles")
+                # compute result
+                result = [sum(self.data[m][n] * rhs.data[n][0]
+                               for n in range(self.shape[1]))
+                          for m in range(self.shape[0])]
+                return Vector(result)
+            else:
+                raise TypeError(f'Wrong type: {rhs}')
 
-    # def __rmul__(self, scalar):
-    #     if self.shape[1] == 1:
-    #         return Vector([[scalar * value[0]] for value in self.values])
-    #     return Vector([[scalar * value for value in self.values[0]]])
+        except (TypeError, AttributeError, ValueError):
+            raise TypeError("Error")
+
+    def __rmul__(self, rhs):
+        """rmul: scalar, vectors and matrixes."""
+        try:
+            if isinstance(rhs, (int, float, complex)):
+                try:
+                    result = [[self.data[m][n] * rhs
+                               for n in range(self.shape[1])]
+                              for m in range(self.shape[0])]
+                except TypeError:
+                    result = [self.data[m] * rhs for m in range(self.shape[0])]
+                return type(self)(result)
+
+            elif type(rhs) == Matrix and type(self) == Matrix:
+                # verify shape compatibility
+                if rhs.shape[1] != self.shape[0]:
+                    raise AttributeError("Shapes are not compatibles")
+                # compute result
+                result = [[sum(rhs.data[m][n] * self.data[n][p]
+                            for n in range(self.shape[1]))
+                           for p in range(rhs.shape[1])]
+                          for m in range(self.shape[0])]
+                return type(self)(result)
+
+            elif type(self) == Vector and type(rhs) == Matrix:
+                # verify shape compatibility
+                return rhs * self
+
+        except (TypeError, AttributeError, ValueError):
+            raise TypeError("Error")
+
+    def T(self):
+        """Transpose matrix (row / column)"""
+        if type(self) == Matrix:
+            return Matrix([[self.data[m][n] for m in range(self.shape[0])]
+                           for n in range(self.shape[1])])
+        elif type(self) == Vector:
+            if self.shape[0] == 1:
+                return Vector([[self.data[n]] for n in range(self.shape[1])])
+            elif self.shape[1] == 1:
+                return Vector([self.data[m][0] for m in range(self.shape[0])])
 
     def __str__(self):
         return f'{self.data}'
 
     def __repr__(self):
         return f'{self.data}'
+
+
+class Vector(Matrix):
+    """Vector class"""
+
+    def __init__(self, data):
+        """Constructor"""
+        # list constructor
+        if isinstance(data, list):
+            n = len(data) if data is not None else 0
+            # row vector
+            if any(isinstance(item, list) for item in data):
+                try:
+                    if not all(len(row) == 1 for row in data):
+                        raise ValueError(f'Wrong shape : {data}')
+                except TypeError:
+                    raise ValueError(f'Wrong shape: {data}')
+                if not all([isinstance(value, (int, float, complex))
+                            for value in row] for row in data):
+                    raise TypeError(f'Wrong type : {data}')
+                self.data = data
+                self.shape = (n, 1 if len(data) != 0 else 0)
+            # column vector
+            else:
+                if not all(isinstance(value, (int, float, complex))
+                           for value in data):
+                    raise TypeError(f'Wrong type : {data}')
+                self.data = data
+                self.shape = (1 if len(data) != 0 else 0, n)
+        else:
+            raise TypeError(f'Wrong type: {data}')
+
+    def dot(self, v):
+        """Dot product"""
+        if v.shape == self.shape:
+            if self.shape[1] == 1:
+                return sum(self.data[i][0] * v.data[i][0]
+                           for i in range(len(self.data)))
+            return sum(self.data[0][i] * v.data[0][i]
+                       for i in range(len(self.data[0])))
+        raise AttributeError("Vectors don't have the same shape")
--- module00/ex00/test.py
+++ module00/ex00/test.py
@@ -1,20 +1,287 @@
 """Test module"""
 from matrix import Matrix
+from matrix import Vector
 
 
 if __name__ == '__main__':
 
-    print("\033[1;35m--Simple matrix creation--\033[0m")
-    print("\033[33mmat = Matrix([[1,2,3], [4,5,6]])\033[0m")
-    mat = Matrix([[1,2,3], [4,5,6]])
-    print(f'\033[1;35m>\033[0m {mat}')
+    # -----------------
+    # CONSTRUCTOR TESTS
+    # -----------------
+    print(f'{"":-<60}')
 
-    print("\n\033[1;35m--Empty matrix creation--\033[0m")
-    print("\033[33mmat = Matrix([[], []])\033[0m")
+    # Creation from list
+    mat = Matrix([[1, 2, 3], [4, 5, 6]])
+    print(f'{"From list":20}: {mat}')
+    print(f'{"":20}: {mat.shape}')
+
+    # Creation with empty list
     mat = Matrix([[], []])
-    print(f'\033[1;35m>\033[0m {mat}')
+    print(f'{"Empty list":20}: {mat}')
+    print(f'{"":20}: {mat.shape}')
 
-    print("\n\033[1;35m--Non square matrix creation--\033[0m")
-    print("\033[33mmat = Matrix([[1,2,3], [4,5,6], [7,8]])\033[0m")
-    mat = Matrix([[1,2,3], [4,5,6], [7,8]])
-    print(f'\033[1;35m>\033[0m {mat}')
+    # From non square list
+    try:
+        mat = Matrix([[1, 2, 3], [4, 5, 6], [7, 8]])
+    except ValueError as exc:
+        print(f'{"Non square list":20}: {exc}')
+
+    # From tuple
+    mat = Matrix((3, 2))
+    print(f'{"From tuple":20}: {mat}')
+    print(f'{"":20}: {mat.shape}')
+
+    # From tuple with 0
+    mat = Matrix((0, 3))
+    print(f'{"From tuple with 0":20}: {mat}')
+    print(f'{"":20}: {mat.shape}')
+
+    # From tuple with 1
+    mat = Matrix((3, 1))
+    print(f'{"From tuple with 1":20}: {mat}')
+    print(f'{"":20}: {mat.shape}')
+
+    # From tuple non int
+    try:
+        mat = Matrix((2.1, 3))
+    except TypeError as exc:
+        print(f'{"From tuple non int":20}: {exc}')
+
+    # -----------------
+    # ADD
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple addition
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    m2 = Matrix([[4, 4, 4], [3, 3, 3], [2, 2, 2]])
+    result = m1 + m2
+    print(f'{"Addition":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong addition
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    m2 = Matrix([[4, 4], [3, 3], [2, 2]])
+    try:
+        result = m1 + m2
+    except AttributeError as exc:
+        print(f'{"Addition":20}: {exc}')
+
+    # -----------------
+    # SUB
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple substraction
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    m2 = Matrix([[4, 4, 4], [3, 3, 3], [2, 2, 2]])
+    result = m1 - m2
+    print(f'{"Substraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong substraction
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    m2 = Matrix([[4, 4], [3, 3], [2, 2]])
+    try:
+        result = m1 - m2
+    except AttributeError as exc:
+        print(f'{"Substraction":20}: {exc}')
+
+    # simple substraction
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    m2 = Matrix([[4, 4, 4], [3, 3, 3], [2, 2, 2]])
+    result = m1.__rsub__(m2)
+    print(f'{"Substraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # -----------------
+    # DIV
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple division
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    result = m1 / 2
+    print(f'{"Division":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong division
+    try:
+        result = m1 / 0
+    except ArithmeticError as exc:
+        print(f'{"Division by 0":20}: {exc}')
+
+    # wrong division
+    try:
+        result = 8 / m1
+    except ArithmeticError as exc:
+        print(f'{"Division of a scalar":20}: {exc}')
+
+    # -----------------
+    # MUL
+    # -----------------
+    print(f'{"":-<60}')
+
+    # scalar multiplication
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    result = m1 * 2
+    print(f'{"Scalar mul":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # vector multiplication
+    m1 = Matrix([[0.0, 1.0, 2.0], [0.0, 2.0, 4.0]])
+    v1 = Vector([[1], [2], [3]])
+    result = m1 * v1
+    print(f'{"Vector mul":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # matrix multiplication
+    m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
+                 [0.0, 2.0, 4.0, 6.0]])
+    m2 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0]])
+    result = m1 * m2
+    print(f'{"Matrix mul":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # rev scalar multiplication
+    m1 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    result = m1.__rmul__(2)
+    print(f'{"Scalar rmul":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # -----------------
+    # TRANSPOSITION
+    # -----------------
+    m1 = Matrix([[0.0, 1.0, 2.0, 3.0],
+                 [0.0, 2.0, 4.0, 6.0]])
+    print(f'{"Transposed":20}: {m1.T()}')
+    print(f'{"":20}: {m1.T().shape}')
+
+    # -----------------
+    # VECTOR CONSTRUCTOR TESTS
+    # -----------------
+    print(f'{"":-<60}')
+
+    # Creation row vector from list
+    vec = Vector([1, 2, 3])
+    print(f'{"From list":20}: {vec}')
+    print(f'{"":20}: {vec.shape}')
+
+    # Creation column vector from list
+    vec = Vector([[1], [2], [3]])
+    print(f'{"Empty list":20}: {vec}')
+    print(f'{"":20}: {vec.shape}')
+
+    # Creation with empty list
+    vec = Vector([])
+    print(f'{"Empty list":20}: {vec}')
+    print(f'{"":20}: {vec.shape}')
+
+    # From non scalar list
+    try:
+        vec = Vector(['toto', 'titi', 'tata'])
+    except TypeError as exc:
+        print(f'{"Non scalar list":20}: {exc}')
+
+    # Creation with wrong list
+    try:
+        vec = Vector([[1], [2], [3]])
+    except (TypeError, ValueError) as exc:
+        print(f'{"Wrong list":20}: {exc}')
+
+    # -----------------
+    # VECTOR ADD
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple addition
+    v1 = Vector([2, 2, 2])
+    v2 = Vector([4, 4, 4])
+    result = v1 + v2
+    print(f'{"Addition":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong addition
+    v1 = Vector([2, 2, 2])
+    v2 = Vector([4, 4])
+    try:
+        result = v1 + v2
+    except AttributeError as exc:
+        print(f'{"Addition":20}: {exc}')
+
+    # -----------------
+    # VECTOR SUB
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple substraction
+    v1 = Vector([2, 2, 2])
+    v2 = Vector([4, 4, 4])
+    result = v1 - v2
+    print(f'{"Substraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # simple substraction 2
+    v1 = Vector([[2], [2], [2]])
+    v2 = Vector([[4], [4], [4]])
+    result = v1 - v2
+    print(f'{"Substraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong substraction
+    v1 = Vector([2, 2, 2, 2])
+    v2 = Vector([4, 4])
+    try:
+        result = v1 - v2
+    except AttributeError as exc:
+        print(f'{"Substraction":20}: {exc}')
+
+    # simple rsubstraction 
+    v1 = Vector([2, 2, 2])
+    v2 = Vector([4, 4, 4])
+    result = v1.__rsub__(v2)
+    print(f'{"rSubstraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # simple rsubstraction 2
+    v1 = Vector([[2], [2], [2]])
+    v2 = Vector([[4], [4], [4]])
+    result = v1.__rsub__(v2)
+    print(f'{"rSubstraction":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # -----------------
+    # VECTOR DIV
+    # -----------------
+    print(f'{"":-<60}')
+
+    # simple division
+    v1 = Vector([2, 2, 2, 3])
+    result = v1 / 2
+    print(f'{"Division":20}: {result}')
+    print(f'{"":20}: {result.shape}')
+
+    # wrong division
+    try:
+        result = v1 / 0
+    except ArithmeticError as exc:
+        print(f'{"Division by 0":20}: {exc}')
+
+    # wrong division
+    try:
+        result = 8 / v1
+    except ArithmeticError as exc:
+        print(f'{"Division of a scalar":20}: {exc}')
+
+    # -----------------
+    # VECTOR TRANSPOSITION
+    # -----------------
+    v1 = Vector([0.0, 1.0, 2.0, 3.0])
+
+    print(f'{"Transposed":20}: {v1.T()}')
+    print(f'{"":20}: {v1.T().shape}')
+
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+    print(f'{"Transposed":20}: {v1.T()}')
+    print(f'{"":20}: {v1.T().shape}')
