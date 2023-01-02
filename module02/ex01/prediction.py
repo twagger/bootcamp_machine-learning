49544248db2b70534fff4bb3406ceb6cde974b80
twagner <twagner@student.42.fr>
2023-01-02 15:48:07 +0100

    init: reinit module02 folders to fit requirements

--- module02/ex01/prediction.py
+++ module02/ex01/prediction.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-def simple_predict(x: np.ndarray, theta: np.ndarray):
+def predict_(x: np.ndarray, theta: np.ndarray):
     """
     Computes the prediction vector y_hat from two non-empty numpy.array.
     Args:
@@ -26,13 +26,9 @@ def simple_predict(x: np.ndarray, theta: np.ndarray):
         if x.shape[1] != theta.shape[0] - 1:
             print('Something went wrong')
             return None
-        # calculation WITHOUT linear algrebra trick
-        x_prime = np.zeros((x.shape[0], 1))
-        for m in range(x.shape[0]):
-            x_prime[m][0] = theta[0]
-            for n in range(x.shape[1]):
-                x_prime[m][0] += theta[n + 1][0] * x[m][n]
-        return x_prime
+        # calculation WITH linear algrebra trick
+        x_prime = np.hstack((np.ones((x.shape[0], 1)), x))
+        return x_prime.dot(theta)
 
     except (ValueError, TypeError, AttributeError) as exc:
         print(exc)
@@ -41,30 +37,30 @@ def simple_predict(x: np.ndarray, theta: np.ndarray):
 
 if __name__ == "__main__":
 
-    x = np.arange(1, 13).reshape((4, -1))
+    x = np.arange(1,13).reshape((4,-1))
 
     # Example 1:
     theta1 = np.array([5, 0, 0, 0]).reshape((-1, 1))
-    print(simple_predict(x, theta1))
+    print(predict_(x, theta1))
     # Ouput:
     # array([[5.], [5.], [5.], [5.]])
     # Do you understand why y_hat contains only 5’s here?
 
     # Example 2:
     theta2 = np.array([0, 1, 0, 0]).reshape((-1, 1))
-    print(simple_predict(x, theta2))
+    print(predict_(x, theta2))
     # Output:
     # array([[ 1.], [ 4.], [ 7.], [10.]])
     # Do you understand why y_hat == x[:,0] here?
 
     # Example 3:
     theta3 = np.array([-1.5, 0.6, 2.3, 1.98]).reshape((-1, 1))
-    print(simple_predict(x, theta3))
+    print(predict_(x, theta3))
     # Output:
     # array([[ 9.64], [24.28], [38.92], [53.56]])
 
     # Example 4:
     theta4 = np.array([-3, 1, 2, 3.5]).reshape((-1, 1))
-    print(simple_predict(x, theta4))
+    print(predict_(x, theta4))
     # Output:
     # array([[12.5], [32. ], [51.5], [71. ]])
