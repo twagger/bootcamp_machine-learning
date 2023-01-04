90796f3ebc0e0873c16586f0650f9fd4017ffb55
twagner <twagner@student.42.fr>
2023-01-04 18:17:00 +0100

    progress: trying to really understand how to do

--- module02/ex10/benchmark_train.py
+++ module02/ex10/benchmark_train.py
@@ -4,21 +4,120 @@ This program performs the training of all the models and save the parameters
 of the different models into a file.
 In models.csv are the parameters of all the models I have explored and trained.
 """
+# general modules
 import numpy as np
-import panda as pd
+import pandas as pd
 import csv
+import matplotlib.pyplot as plt
+# user modules
+import sys
+sys.path.insert(1, '../ex09/')
+from data_spliter import data_spliter
+sys.path.insert(1, '../ex07/')
+from polynomial_model import add_polynomial_features
+sys.path.insert(1, '../ex05')
+from mylinearregression import MyLinearRegression as MyLR
 
 
+
+# Helper functions
+def polynomial_matrix(x: np.ndarray, degree: int) -> np.ndarray:
+    """return the polynomial matrix for x"""
+    x_ = np.empty((x.shape[0], 0))
+    for feature in range(x.shape[1]):
+        x_ = np.hstack((x_, add_polynomial_features(x[:, feature], degree)))
+    return x_
+
+
+def save_training(writer, nb_model: int, degree: int, thetas: np.ndarray,
+                  alpha: float, max_iter: int, loss: float):
+    """save the training in csv file"""
+    f_thetas = '[' + ', '.join(str(t) for t in my_lr.thetas.flatten()) + ']'
+    writer.writerow([nb_model, degree, f_thetas, alpha, max_iter, loss])
+
+
+def z_score(x: np.ndarray) -> np.ndarray:
+    """
+    Computes the normalized version of a non-empty numpy.ndarray using the
+        z-score standardization.
+    Args:
+        x: has to be an numpy.ndarray, a vector.
+    Returns:
+        x' as a numpy.ndarray.
+        None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
+    Raises:
+        This function shouldn't raise any Exception.
+    """
+    try:
+        # type test
+        if not isinstance(x, np.ndarray):
+            print("Something went wrong")
+            return None
+        # shape test
+        x = x.reshape((-1, 1))
+        # normalization
+        z_score_formula = lambda x, std, mean: (x - mean) / std
+        zscore_normalize = np.vectorize(z_score_formula)
+        x_prime = zscore_normalize(x, np.std(x), np.mean(x))
+        return x_prime
+
+    except (ValueError, TypeError, AttributeError) as exc:
+        print(exc)
+        return None
+
+
+def minmax(x: np.ndarray) -> np.ndarray:
+    """
+    Computes the normalized version of a non-empty numpy.ndarray using the
+        min-max standardization.
+    Args:
+        x: has to be an numpy.ndarray, a vector.
+    Returns:
+        x' as a numpy.ndarray.
+        None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
+    Raises:
+        This function shouldn't raise any Exception.
+    """
+    try:
+        # type test
+        if not isinstance(x, np.ndarray):
+            print("Something went wrong")
+            return None
+        # shape test
+        x = x.reshape((-1, 1))
+        # normalization
+        min_max_formula = lambda x, min, max: (x - min) / (max - min)
+        minmax_normalize = np.vectorize(min_max_formula)
+        x_prime = minmax_normalize(x, np.min(x), np.max(x))
+        return x_prime
+
+    except (ValueError, TypeError, AttributeError) as exc:
+        print(exc)
+        return None
+
+
+def normalize_xset(x: np.ndarray) -> np.ndarray:
+    """Normalize each feature an entire set of data"""
+    x_norm = np.empty((x.shape[0], 0))
+    for feature in range(x.shape[1]):
+        x_norm = np.hstack((x_norm, minmax(x[:, feature])))
+    return x_norm
+
+
+def plot_features(x: np.ndarray, y: np.ndarray):
+    """plot each x feature versus y"""
+    plt.figure()
+    for feature in range(x.shape[1]):
+        plt.subplot(1, x.shape[1], feature + 1)
+        plt.grid()
+        plt.title(f'{dataset.columns.tolist()[feature + 1]}')
+        plt.scatter(x[:, feature], y)
+        plt.ylabel(f'{dataset.columns.tolist()[-1]}')
+    plt.show()
+
+
+# main program
 if __name__ == "__main__":
-
-    # import necessary modules
-    import sys
-    sys.path.insert(1, '../ex09/')
-    from data_spliter import data_spliter
-    sys.path.insert(1, '../ex07/')
-    from polynomial_model import add_polynomial_features
-    sys.path.insert(1, '../ex05')
-    from mylinearregression import MyLinearRegression as MyLR
 
     # read the dataset
     dataset = pd.read_csv("./space_avocado.csv")
@@ -29,47 +128,66 @@ if __name__ == "__main__":
         writer.writerow(["model", "degree", "thetas", "alpha", "max_iter",
                          "split"])
 
-        # create multiple training set and a test set to optimize the chance of
-        # having a good result
+        # create x matrix and y vector
+        x = np.array(dataset[['weight', 'prod_distance',
+                            'time_delivery']]).reshape((-1, 3))
+        y = np.array(dataset['target']).reshape((-1, 1))
+        x_train, x_test, y_train, y_test = data_spliter(x, y, 0.8)
+
+        # feature scaling to help gradient descend
+        x_train_norm = normalize_xset(x_train)
+
+        # plot every feature with Y to have a global understanding
+        plot_features(x_train, y_train)
+
+        # Trains 4 separate Linear Regression models with polynomial
+        # hypothesis with degrees ranging from 1 to 4
         nb_model = 0
-        for split in range(1, 4):
+        for degree in range(1, 3):
 
-            X = np.array(dataset['weight', 'prod_distance',
-                                'time_delivery']).reshape((-1, 3))
-            y = np.array(dataset['target']).reshape((-1, 1))
-            x_train, x_test, y_train, y_test = data_spliter(X, y, split * 0.2)
+            # global params
+            theta = np.zeros((degree * x_train.shape[1] + 1, 1))
+            alpha = 0.001
+            max_iter = 1
+            # specific params
+            if degree == 1:
+                alpha = 0.666
+                max_iter = 1
 
-            # Trains 4 separate Linear Regression models with polynomial
-            # hypothesis with degrees ranging from 1 to 4
-            for degree in range(1, 5):
+            # add polynomial features for each feature
+            x_train_ = polynomial_matrix(x_train_norm, degree)
 
-                # global params
-                theta = np.ones(degree + 1).reshape(-1,1)
-                alpha = 2.5e-5
-                max_iter = 500000
-                # specific params
-                if degree == 10:
-                    theta = np.array([[-20.0],[ 160],[ -80],[ 10],
-                                    [ -1]]).reshape(-1,1)
-                    alpha = 2.5e-8
-                    max_iter = 500000
+            # create the model
+            my_lr = MyLR(theta, alpha = alpha, max_iter = max_iter)
+            nb_model += 1
 
-                # add polynomial data
-                x_ = add_polynomial_features(X, degree)
-                # create the model
-                my_lr = MyLR(theta, alpha = alpha, max_iter = max_iter)
-                nb_model += 1
-                # fit the model
-                my_lr.fit_(x_, y)
-                # predict
-                y_hat = my_lr.predict_(x_)
-                # Evaluates and prints evaluation score (MSE) of each model
-                mse = my_lr.mse_(y, y_hat)
-                # saving parameters and results in the models.csv file
-                f_thetas = '|'.join(str(t) for t in my_lr.thetas.flatten())
-                writer.writerow([nb_model,
-                                 degree,
-                                 f_thetas,
-                                 alpha,
-                                 max_iter,
-                                 split * 0.2])
+            # fit the model and store loss each time to observe progress
+            losses = []
+            it = []
+            for iteration in range(50000):
+                my_lr.fit_(x_train_, y_train)
+                x_test_ = polynomial_matrix(x_test, degree)
+                y_hat_test = my_lr.predict_(x_test_)
+                losses.append(my_lr.loss_(y_test, y_hat_test))
+                it.append(iteration)
+
+            # plot the learning curve
+            plt.figure()
+            plt.grid()
+            plt.title(f'Learning curve with alpha = {alpha}')
+            plt.plot(it, losses)
+            plt.xlabel('number of iteration')
+            plt.ylim([min(losses) * 1.00001, max(losses) * 1.00001])
+            plt.ylabel('Loss')
+            plt.show()
+
+            # predict and add polynomial features for each test feature
+            x_test_ = polynomial_matrix(x_test, degree)
+            y_hat_test = my_lr.predict_(x_test_)
+
+            # Evaluates and prints evaluation score (loss) of each model
+            loss = my_lr.loss_(y_test, y_hat_test)
+
+            # saving parameters and results in the models.csv file
+            save_training(writer, nb_model, degree, my_lr.thetas, alpha,
+                            max_iter, loss)
